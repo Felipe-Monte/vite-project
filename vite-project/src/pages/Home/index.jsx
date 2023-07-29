94d8b8b9d4f7b95796502b29c8248feb0da3d9f5
Carlos Felipe <109633306+Felipe-Monte@users.noreply.github.com>
2023-07-28 22:42:07 -0300

    create Notes and page SignIn

--- vite-project/src/pages/Home/index.jsx
+++ vite-project/src/pages/Home/index.jsx
@@ -1,7 +1,11 @@
+import { FiPlus, FiSearch } from 'react-icons/fi'
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
 
 import { Header } from '../../components/Header'
 import { ButtonText } from '../../components/ButtonText'
+import { Section } from '../../components/Section'
+import { Note } from '../../components/Note'
+import { Input } from '../../components/Input'
 
 export function Home() {
   return (
@@ -20,14 +24,24 @@ export function Home() {
       </Menu>
 
       <Search>
-
+        <Input placeholder='Pesquisar pelo titulo' icon={FiSearch} />
       </Search>
 
       <Content>
-
+        <Section title='Minhas notas'>
+          <Note data={{
+            title: 'React',
+            tags: [
+              { id: '1', name: 'Nodejs' },
+              { id: '2', name: 'React' }
+            ]
+          }}
+          />
+        </Section>
       </Content>
 
       <NewNote>
+        <FiPlus />
         Criar nota
       </NewNote>
 
