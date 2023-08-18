9a2e27e8899389fb1ccf2d542188ba7babef967b
Carlos Felipe <carlosfelipemonte1997@outlook.com>
2023-08-18 20:12:29 -0300

    verify user login

--- vite-project/src/pages/SignUp/index.jsx
+++ vite-project/src/pages/SignUp/index.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import { FiMail, FiLock, FiUser } from 'react-icons/fi'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+
+import { api } from '../../../../estudos node/src/services/api'
 
 import { Container, Form, BackgroundImg } from './styles'
 
@@ -13,8 +15,26 @@ export function SignUp() {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
 
+  const navigate = useNavigate()
+
   function handleSignUp() {
-    console.log(name, email, password)
+    if (!name || !email || !password) {
+      return alert('Preencha todos os campos!')
+    }
+
+    api.post("/users", { name, email, password })
+      .then(() => {
+        alert("Usuário cadastrado com sucesso!")
+        navigate("/")
+      })
+      .catch(error => {
+        if (error.response) {
+          alert(error.response.data.message)
+        } else {
+          alert("Não foi possível cadastrar")
+        }
+      })
+
   }
 
   return (
