9ef5916bb9b4f6b61111b7bfc92fb759a9fb1ca1
Carlos Felipe <carlosfelipemonte1997@outlook.com>
2023-08-25 23:24:43 -0300

    img user

--- vite-project/src/components/Header/index.jsx
+++ vite-project/src/components/Header/index.jsx
@@ -1,19 +1,23 @@
 import { RiShutDownLine } from 'react-icons/ri'
 import { Container, Profile, Logout } from './styles'
 
+import {api} from '../../../../estudos node/src/services/api'
+
 import { useAuth } from '../../hooks/auth'
 
 export function Header() {
-  const { signOut } = useAuth()
+  const { signOut, user } = useAuth()
+
+  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
 
   return (
     <Container>
       <Profile to='/profile'>
-        <img src="https://github.com/Felipe-Monte.png" alt="foto do usuário" />
+        <img src={avatarUrl} alt={user.name} />
 
         <div>
           <span>Bem vindo</span>
-          <strong>Carlos Felipe</strong>
+          <strong>{user.name}</strong>
         </div>
       </Profile>
 
--- vite-project/src/pages/Profile/index.jsx
+++ vite-project/src/pages/Profile/index.jsx
@@ -4,6 +4,9 @@ import { Container, Form, Avatar } from './styles'
 
 import { useAuth } from '../../hooks/auth'
 
+import { api } from '../../../../estudos node/src/services/api'
+import avatarPlaceholder from '../../assets/avatar_placeholder.svg'
+
 import { Link } from 'react-router-dom'
 
 import { Input } from '../../components/Input'
@@ -17,6 +20,11 @@ export function Profile() {
   const [passwordOld, setPasswordOld] = useState()
   const [passwordNew, setPasswordNew] = useState()
 
+  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
+
+  const [avatar, setAvatar] = useState(avatarUrl)
+  const [avatarFile, setAvatarFile] = useState()
+
   async function handleUpdate() {
     const user = {
       name,
@@ -25,7 +33,16 @@ export function Profile() {
       old_password: passwordOld,
     }
 
-    await updateProfile({ user })
+    await updateProfile({ user, avatarFile })
+  }
+
+  function handleChangeAvatar(event) {
+    const file = event.target.files[0]
+    setAvatarFile(file)
+
+    const imagePreview = URL.createObjectURL(file)
+    setAvatar(imagePreview)
+
   }
 
   return (
@@ -38,7 +55,7 @@ export function Profile() {
 
       <Form>
         <Avatar>
-          <img src="https://github.com/Felipe-Monte.png" alt="Foto do usuário" />
+          <img src={avatar} alt="Foto do usuário" />
 
           <label htmlFor="avatar">
             <FiCamera />
@@ -46,6 +63,7 @@ export function Profile() {
             <input
               id='avatar'
               type='file'
+              onChange={handleChangeAvatar}
             />
 
           </label>
